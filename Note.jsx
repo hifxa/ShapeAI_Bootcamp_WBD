a58c16b41ff7f56f89cd09a9d6b441867817f75a
hifxa <78956730+hifxa@users.noreply.github.com>
2021-06-28 14:51:59 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web dev program with hands on project</p>
+    </div>
+  );
+}
+export default Info;
